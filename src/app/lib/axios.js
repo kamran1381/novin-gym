33b9226df360a93d8a8e24288a2e8ae161742bd1
kamran <ka6471381@gmail.cm>
2024-07-20 21:05:38 +0330

    furthur deploy changes

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXTAUTH_AXIOS_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_AXIOS_URL;
 
 // axios.interceptors.request.use(
 //   (config) => {
